f8e0de282cdfaa101cc15d423bbb44613e75dc9c
Maedeh <yazdian.oul@gmail.com>
2022-03-16 13:59:57 +0330

    move data from activity to singleton

--- app/src/main/java/ir/homework/geoquiz/GeoQuiz.kt
+++ app/src/main/java/ir/homework/geoquiz/GeoQuiz.kt
@@ -3,4 +3,17 @@ package ir.homework.geoquiz
 object GeoQuiz {
     val questionList = mutableListOf<Question>()
     var num = 0
+
+    fun setData() {
+        questionList.add(Question("Birjand is the capital of North Khorasan.", false))
+        questionList.add(Question("Tehran is the capital of Iran.", true))
+        questionList.add(Question("There is no sea in Yazd.", true))
+        questionList.add(Question("Himalaya is in Iran.", false))
+        questionList.add(Question("Persian Gulf is in Iran.", true))
+        questionList.add(Question("Tehran and Qom are neighbours.", true))
+        questionList.add(Question("There is enough water for many years.", false))
+        questionList.add(Question("Shiraz is the capital of Fars.", true))
+        questionList.add(Question("Ahvaz and Esfahan are neighbours.", false))
+        questionList.add(Question("Shomal is the capital of Iran.", false))
+    }
 }
