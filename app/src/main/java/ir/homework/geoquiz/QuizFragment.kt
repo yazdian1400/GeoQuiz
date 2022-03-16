df029544281046bff2d7de48b474ffddfd364944
Maedeh <yazdian.oul@gmail.com>
2022-03-16 16:00:14 +0330

    add view model to QuizFragment.kt

--- app/src/main/java/ir/homework/geoquiz/QuizFragment.kt
+++ app/src/main/java/ir/homework/geoquiz/QuizFragment.kt
@@ -9,18 +9,18 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import ir.homework.geoquiz.databinding.FragmentQuizBinding
 
 class QuizFragment : Fragment() {
     lateinit var binding: FragmentQuizBinding
     var hasCheated = false
+    private lateinit var vModel: GeoQuizViewModel
 
     companion object{
         const val NUM_OF_QUESTIONS = 10
-        const val NUM = "num"
-        const val CHEATING_ARRAY = "cheatingArray"
-        const val USER_ANSWER_ARRAY = "UserAnswerArray"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,9 +40,9 @@ class QuizFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         if (arguments?.getBoolean("hasCheated") != null) {
             hasCheated = arguments?.getBoolean("hasCheated") as Boolean
-            if (hasCheated) GeoQuiz.questionList[GeoQuiz.num].hasCheated = true
+            if (hasCheated) vModel.questionList[vModel.num].hasCheated = true
         }
-        GeoQuiz.setData()
+        vModel = ViewModelProvider(this)[GeoQuizViewModel::class.java]
         initViews()
         onClickListeners()
     }
@@ -56,37 +56,37 @@ class QuizFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.M)
     private fun onClickListeners() {
         binding.btnTrue.setOnClickListener {
-            GeoQuiz.questionList[GeoQuiz.num].userAnswer = UserAnswer.TRUE
+            vModel.questionList[vModel.num].userAnswer = UserAnswer.TRUE
             binding.btnTrue.isEnabled = false
             binding.btnTrue.setBackgroundColor(getColor(requireContext(),R.color.pink_dark_A100))
             binding.btnFalse.isEnabled = false
             binding.btnFalse.setBackgroundColor(getColor(requireContext(),R.color.blue_dark_A100))
             binding.btnCheat.isEnabled = false
             binding.btnCheat.setBackgroundColor(getColor(requireContext(),R.color.red_dark_A100))
-            messageAnswer(GeoQuiz.questionList[GeoQuiz.num].answer)
+            messageAnswer(vModel.questionList[vModel.num].answer)
         }
         binding.btnFalse.setOnClickListener {
-            GeoQuiz.questionList[GeoQuiz.num].userAnswer = UserAnswer.FALSE
+            vModel.questionList[vModel.num].userAnswer = UserAnswer.FALSE
             binding.btnTrue.isEnabled = false
             binding.btnTrue.setBackgroundColor(getColor(requireContext(),R.color.pink_dark_A100))
             binding.btnFalse.isEnabled = false
             binding.btnFalse.setBackgroundColor(getColor(requireContext(),R.color.blue_dark_A100))
             binding.btnCheat.isEnabled = false
             binding.btnCheat.setBackgroundColor(getColor(requireContext(),R.color.red_dark_A100))
-            messageAnswer(!GeoQuiz.questionList[GeoQuiz.num].answer)
+            messageAnswer(!vModel.questionList[vModel.num].answer)
         }
         binding.btnNext.setOnClickListener {
-            GeoQuiz.num++
+            vModel.num++
             changePage()
         }
         binding.btnPrev.setOnClickListener {
-            GeoQuiz.num--
+            vModel.num--
             changePage()
         }
         binding.btnCheat.setOnClickListener {
             val bundle = Bundle()
             //bundle.putBoolean("answer",GeoQuiz.questionList[GeoQuiz.num].answer)
-            bundle.putInt("num",GeoQuiz.num)
+            bundle.putInt("num",vModel.num)
             findNavController().navigate(R.id.cheating_action,bundle)
         }
     }
@@ -99,7 +99,7 @@ class QuizFragment : Fragment() {
     fun messageAnswer(boolean: Boolean){
         if (boolean){
             Toast.makeText(requireContext(), "Correct", Toast.LENGTH_LONG).show()
-            if (GeoQuiz.questionList[GeoQuiz.num].hasCheated == true) Toast.makeText(requireContext(), "Cheating is Wrong.",
+            if (vModel.questionList[vModel.num].hasCheated) Toast.makeText(requireContext(), "Cheating is Wrong.",
                 Toast.LENGTH_LONG).show()
         }
         else    Toast.makeText(requireContext(), "Incorrect", Toast.LENGTH_LONG).show()
@@ -107,17 +107,17 @@ class QuizFragment : Fragment() {
 
     @RequiresApi(Build.VERSION_CODES.M)
     fun changePage() {
-        binding.tvQuestion.text = GeoQuiz.questionList[GeoQuiz.num].question
-        binding.btnPrev.isEnabled = GeoQuiz.num != 0
-        binding.btnNext.isEnabled = GeoQuiz.num != (NUM_OF_QUESTIONS - 1)
-        binding.btnFalse.isEnabled = GeoQuiz.questionList[GeoQuiz.num].userAnswer == UserAnswer.NO_ANSWER
-        binding.btnTrue.isEnabled = GeoQuiz.questionList[GeoQuiz.num].userAnswer == UserAnswer.NO_ANSWER
-        binding.btnCheat.isEnabled = GeoQuiz.questionList[GeoQuiz.num].userAnswer == UserAnswer.NO_ANSWER
-        if (GeoQuiz.num != 0)   binding.btnPrev.setBackgroundColor(getColor(requireContext(),R.color.green_A100))
+        binding.tvQuestion.text = vModel.questionList[vModel.num].question
+        binding.btnPrev.isEnabled = vModel.num != 0
+        binding.btnNext.isEnabled = vModel.num != (NUM_OF_QUESTIONS - 1)
+        binding.btnFalse.isEnabled = vModel.questionList[vModel.num].userAnswer == UserAnswer.NO_ANSWER
+        binding.btnTrue.isEnabled = vModel.questionList[vModel.num].userAnswer == UserAnswer.NO_ANSWER
+        binding.btnCheat.isEnabled = vModel.questionList[vModel.num].userAnswer == UserAnswer.NO_ANSWER
+        if (vModel.num != 0)   binding.btnPrev.setBackgroundColor(getColor(requireContext(),R.color.green_A100))
         else    binding.btnPrev.setBackgroundColor(getColor(requireContext(),R.color.green_dark_A100))
-        if (GeoQuiz.num != NUM_OF_QUESTIONS - 1)  binding.btnNext.setBackgroundColor(getColor(requireContext(),R.color.green_A100))
+        if (vModel.num != NUM_OF_QUESTIONS - 1)  binding.btnNext.setBackgroundColor(getColor(requireContext(),R.color.green_A100))
         else    binding.btnNext.setBackgroundColor(getColor(requireContext(),R.color.green_dark_A100))
-        if (GeoQuiz.questionList[GeoQuiz.num].userAnswer == UserAnswer.NO_ANSWER) {
+        if (vModel.questionList[vModel.num].userAnswer == UserAnswer.NO_ANSWER) {
             binding.btnFalse.setBackgroundColor(getColor(requireContext(),R.color.blue_A100))
             binding.btnTrue.setBackgroundColor(getColor(requireContext(),R.color.pink_A100))
             binding.btnCheat.setBackgroundColor(getColor(requireContext(),R.color.red_A100))
@@ -127,8 +127,8 @@ class QuizFragment : Fragment() {
             binding.btnTrue.setBackgroundColor(getColor(requireContext(),R.color.pink_dark_A100))
             binding.btnCheat.setBackgroundColor(getColor(requireContext(),R.color.red_dark_A100))
         }
-        if ((GeoQuiz.questionList[GeoQuiz.num].userAnswer == UserAnswer.TRUE && GeoQuiz.questionList[GeoQuiz.num].answer
-                    || GeoQuiz.questionList[GeoQuiz.num].userAnswer == UserAnswer.FALSE && !GeoQuiz.questionList[GeoQuiz.num].answer) && GeoQuiz.questionList[GeoQuiz.num].hasCheated)
+        if ((vModel.questionList[vModel.num].userAnswer == UserAnswer.TRUE && vModel.questionList[vModel.num].answer
+                    || vModel.questionList[vModel.num].userAnswer == UserAnswer.FALSE && !vModel.questionList[vModel.num].answer) && vModel.questionList[vModel.num].hasCheated)
             Toast.makeText(requireContext(), "Cheating is wrong.", Toast.LENGTH_LONG).show()
     }
 }
